2699a8890254700b5a7a650d5ce2cc27fa651e22
milag <milag@NOTE>
2026-09-01 20:27:36 -0300

    style: mover versao para o rodape do app

--- outputs/nap-pwa/sw.js
+++ outputs/nap-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soneca-pwa-v234";
+const CACHE_NAME = "soneca-pwa-v235";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,8 +11,8 @@ const ASSETS = [
   "./vendor/fontawesome/webfonts/fa-brands-400.ttf",
   "./vendor/fontawesome/webfonts/fa-v4compatibility.woff2",
   "./vendor/fontawesome/webfonts/fa-v4compatibility.ttf",
-  "./styles.css?v=115",
-  "./app.js?v=209",
+  "./styles.css?v=116",
+  "./app.js?v=210",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
